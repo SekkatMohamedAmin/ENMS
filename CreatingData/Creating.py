b5052aa24f22b9b8abb148b4dc1c6645faed22a8
Sekkat Mohamed Amin <mohamed.amin.sekkat@ensia.edu.dz>
2023-12-21 04:05:37 +0100

    adding json file of cities and districts plus customerBilling.html

--- CreatingData/Creating.py
+++ CreatingData/Creating.py
@@ -6,7 +6,20 @@ names = ["ahmed", "ali" , "abdouAllah" ,  " abdou" , "abdelkader" , "abdelhak" ,
 #algerien last names
 lastnames = ["hammoudi" , "hammouda" , "hammo" , "hammou" , "hammouche" , "hammoud" , "hammoum" , "hammoumi" , "hammouche" , "hammoudene" , "hammoudeh" , "hammoudi" , "hammouda" , "hammoud" , "maachou" , "maachou" , "maachouk" ,"marbouh" , "rabah" , "aghiles" , "gechhir" , "dechirri" ,
              "dschirri" , "gechir" , "gechiri" , "gechirri" , "gechirry" , "gechir","medjouj","belhout","horregmi","merrad",]
-cities = [""]
+cities =  [
+    "Algiers", "Oran", "Constantine", "Annaba", "Batna",
+    "Blida", "Setif", "Chlef", "Djelfa", "Biskra",
+    "Tebessa", "Tlemcen", "Ouargla", "Bejaia", "Skikda",
+    "Tiaret", "Tizi Ouzou", "El Oued", "Béchar", "Mostaganem",
+    "Bordj Bou Arreridj", "El Achir", "Médéa", "Sidi Bel Abbès",
+    "Laghouat", "Mascara", "Tizi Gheniff", "Tiaret", "Oum el Bouaghi",
+    "Béjaïa", "Ain Temouchent", "Guelma", "Relizane", "Tamanrasset",
+    "El Bayadh", "Ghardaia", "Illizi", "Bechar", "Adrar",
+    "Ain Defla", "Tindouf", "Bouira", "El Taref", "Souk Ahras",
+    "Ghardaïa", "Mila", "Laghouat", "Tissemsilt", "Tizi Ouzou",
+    "Relizane", "Jijel", "Boumerdes", "Tipaza", "Skikda",
+    "Tébessa", "Béchar", "Adrar", "Djanet"
+]
 
 file = open("AlgerienCustomers.csv", "w")
 id = 1
--- Website/main.cpp
+++ Website/main.cpp
@@ -8,6 +8,31 @@ int main(){
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
     });
+
+
+    app.route_dynamic("/submit")
+    .methods("POST"_method)
+    ([&](const crow::request& req, crow::response& res) {
+        crow::mustache::context ctx;
+        
+        // Access multiple form data parameters
+        auto formData1 = req.url_params.get("param1");
+        auto formData2 = req.url_params.get("param2");
+        
+        // Call function in Customer class
+        Customer customer;
+        std::string result = "test" );
+
+        // Add the result to the Mustache context
+        ctx["result"] = result;
+
+        // Render Mustache template
+        auto page = crow::mustache::load("path/to/template.html");
+        res.write(page.render(ctx));
+        res.end();
+    });
+
+
     app.port(18080).multithreaded().run();
     return 0;
 
